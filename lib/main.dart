c64e3277954bcf6e7e993fe3e297cb7de05238a5
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2022-07-03 20:04:19 +0800

    master

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Uas Flutter',
+      title: 'Uas Mobile Programing Lanjutan',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
